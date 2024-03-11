f798f3a83598213b8f571bf0c29bbd6a98d62b8a
digitalvasanth <digitalvasanth01@gmail.com>
2024-03-11 23:39:54 +0530

    commit done

---
+++ LineTrip.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class LineTrip {
+    public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int max=Integer.MIN_VALUE;
+        while(t-->0)
+        {
+            int last=0;
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<arr.length;i++)
+            {
+                arr[i]=sc.nextInt();
+
+            }
+
+            for(int i=0;i<arr.length;i++)
+            {
+                max=Math.max(max,arr[i]-last);
+                last=arr[i];
+
+            }
+            max=Math.max(max,2*(x-last));
+
+        }
+        System.out.println(max);
+    }
+    
+}
